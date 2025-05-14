89f2074362daebafa21d33e51d928c9e5dd167c7
QacMauri <berenguermauricio10@gmail.com>
2025-05-14 08:08:03 -0300

    Primer commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF6B4339);
+
+const Color kPrimaryDark = Color(0xFF6B4339);
+const Color kPrimaryMedium = Color(0xFF52342C);
+const Color kPrimaryLight = Color(0xFF8B7268);
+const Color kPrimaryGradientEnd = Color(0xFF7D7472);
+const Color kSecondaryGlow = Color(0xFFE2BDAD);
